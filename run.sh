802685964bf4eb6e750603d8546bd26bf4bce936
Pedro H. Spagiari <pedro.spagiari@vivareal.com>
2016-12-01 20:19:42 -0200

    missing path

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 MUTEX=$(ps aux|grep -q "python huna_crawler.p[y]")
 if [[ $? -eq 1 ]]; then
-	python huna_crawler.py &
+	python /root/web-regex-crawler/huna_crawler.py &
 else
 	echo "Already Running"
 fi
